72a2b4fb3e28b365fbe480889b88d444595da90d
Tikkaaa3 <tikkaaa3@gmail.com>
2025-06-04 17:31:01 +0200

    Maze Solver is Completed

--- cell.py
+++ cell.py
@@ -2,16 +2,17 @@ from graphics import Line, Point
 
 
 class Cell:
-    def __init__(self, window):
+    def __init__(self, win=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+        self.visited = False
         self.__x1 = -1
         self.__x2 = -1
         self.__y1 = -1
         self.__y2 = -1
-        self.__win = window
+        self.__win = win
 
     def draw(self, x1, y1, x2, y2):
         if self.__win is None:
@@ -23,15 +24,27 @@ class Cell:
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self.__win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self.__win.draw_line(line, "white")
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self.__win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self.__win.draw_line(line, "white")
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
             self.__win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self.__win.draw_line(line, "white")
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self.__win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self.__win.draw_line(line, "white")
 
     def draw_move(self, to_cell, undo=False):
         half_length = abs(self.__x2 - self.__x1) // 2
